0fd79bcdfa07805e756b5b049c3c132274e23847
Maryam Khan <maryam.b.khan@edu.sait.ca>
2022-04-27 16:39:36 -0600

    added project page

--- pages/index.vue
+++ pages/index.vue
@@ -28,6 +28,8 @@
         </svg>
       </section>
       <SkillsCards />
+      <p class="text-xl text-center mt-8 text-pink-800 text">You want to learn more <button  type="submit" id="submit" class="hover:bg-pink-900 bg-pink-800 text-white p-2 text-2xl rounded-md mt-4 text"><a href="/about"> about me</a></button></p>
+     
     </main>
     <TheFooter />
   </div>
---
+++ pages/project.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <TheHeader />
+    <main class="flex flex-col min-h-screen"></main>
+    <TheFooter />
+  </div>
+</template>
